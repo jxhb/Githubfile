fc84fa44f958ef611e203256b344ec7ac1cbd135
jxhb <3226473822@qq.com>
2019-08-15 15:52:35 +0800

    新分支提交

--- src/hello/helloGit.java
+++ src/hello/helloGit.java
@@ -4,5 +4,8 @@ public class helloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git,I come from IDEA");
         System.out.println("welcome!!! Nice to meet you...");
+        for (int i = 0; i < 3; i++) {
+            System.out.printf("this is good!!!!!!  ");
+        }
     }
 }
