779021fb29290fe79f76014708a38d72d58bacb2
jxhb <3226473822@qq.com>
2019-08-15 15:54:53 +0800

    正确的新分支添加

--- src/hello/helloGit.java
+++ src/hello/helloGit.java
@@ -4,5 +4,6 @@ public class helloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git,I come from IDEA");
         System.out.println("welcome!!! Nice to meet you...");
+        System.out.println("welcome!! 新分支来了");
     }
 }
