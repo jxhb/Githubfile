ab9910539dbeda1ae7ab5bcc1e1e3608fc2d0838
jxhb <3226473822@qq.com>
2019-08-15 15:37:44 +0800

    Second commit:add "welcome! Nice to meet you."

--- src/hello/helloGit.java
+++ src/hello/helloGit.java
@@ -3,5 +3,6 @@ package hello;
 public class helloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git,I come from IDEA");
+        System.out.println("welcome!!! Nice to meet you...");
     }
 }
